f005fe03c645e7b01b61010ce12e963a3b362f69
LinjianLi <linjiann.li@gmail.com>
2019-10-11 17:24:17 +0800

    Add Submodule ARFF and Test It

---
+++ test/test_arff.cpp
@@ -0,0 +1,13 @@
+//
+// Created by LinjianLi on 2019/10/11.
+//
+
+#include "gtest/gtest.h"
+#include "arff_parser.h"
+
+TEST(ArffTest, sometest) {
+  ArffParser ap = ArffParser("../../data/dataset/Phishing/Training Dataset.arff");
+  auto ap_data = ap.parse();
+  EXPECT_EQ(ap_data->get_relation_name(), "phishing");
+  EXPECT_EQ(ap_data->num_attributes(), 31);
+}
