6633129ce05842801792d02ed58b4e35682a3b73
jjiantong <jjiantong@gmail.com>
2023-10-12 21:58:14 +0800

    add submodule Google Test

---
+++ test/test_main.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+TEST(SampletTest, sample) {
+    EXPECT_EQ(1, 1);
+}
