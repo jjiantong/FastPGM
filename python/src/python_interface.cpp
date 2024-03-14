534091e19275fed2849d9bf9eaa9eee607a3bcb9
jjiantong <jjiantong@gmail.com>
2024-03-14 11:14:06 +0800

    add python interface test

--- python/src/python_interface.cpp
+++ python/src/python_interface.cpp
@@ -1,9 +1,9 @@
 #include <pybind11/pybind11.h>
-//#include "fastbn/Parameter.h"
+#include "fastbn/Parameter.h"
 
-int add(int i, int j) {
-    return i + j;
-}
+//int add(int i, int j) {
+//    return i + j;
+//}
 
 namespace py = pybind11;
 
