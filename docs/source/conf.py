54008d91dd46861d6f00e9cb2bdcf59de009e315
jjiantong <jjiantong@gmail.com>
2024-03-27 11:50:22 +0800

    fix read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = 'latest'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark'
+extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
